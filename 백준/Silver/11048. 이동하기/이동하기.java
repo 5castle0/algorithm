5d085922afe72a36b94e503ed5f7babdd358b172
5castle0 <62434758+5castle0@users.noreply.github.com>
2025-07-14 11:38:02 +0900

    [Silver II] Title: 이동하기, Time: 464 ms, Memory: 75008 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][m];
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<m; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i=1; i<n;i++){
+            arr[i][0] += arr[i-1][0];
+        }
+        for(int i=1; i<m; i++){
+            arr[0][i] += arr[0][i-1];
+        }
+
+
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                arr[i][j] += Math.max(arr[i-1][j-1], Math.max(arr[i-1][j], arr[i][j-1]));
+            }
+        }
+
+        bw.write(String.valueOf(arr[n-1][m-1]));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
