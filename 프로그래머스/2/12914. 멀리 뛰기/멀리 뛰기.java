7d7cda2b803459031f2ccd50d11b0a7e190f667f
5castle0 <62434758+5castle0@users.noreply.github.com>
2026-05-21 17:22:09 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.09 ms, Memory: 61.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long solution(int n) {
+        if (n == 1) return 1L;
+        if (n == 2) return 2L;
+        
+        long[] dp = new long[n+1];
+        dp[1] = 1L;
+        dp[2] = 2L;
+        for(int i=3; i<=n;i++){
+            dp[i] = (dp[i-1] + dp[i-2])%1234567;    
+        }
+        
+        long answer = dp[n];
+        return answer;
+    }
+}
