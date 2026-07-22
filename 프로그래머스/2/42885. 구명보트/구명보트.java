da9a840340dc1c329999585e311c63a4d3899195
5castle0 <62434758+5castle0@users.noreply.github.com>
2026-07-22 16:05:51 +0900

    [level 2] Title: 구명보트, Time: 9.33 ms, Memory: 59.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+    
+        int i = 0;
+        
+        for(int j=people.length-1; j>=i; j--){
+            if(people[j] + people[i] <= limit){
+                i++;
+            }    
+            answer++;
+        }
+        
+        return answer;
+    }
+}
