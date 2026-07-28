0c78d9fa5b9698f60548846ef5a28d0d22697f67
5castle0 <62434758+5castle0@users.noreply.github.com>
2026-07-28 10:53:03 +0900

    [level 2] Title: 롤케이크 자르기, Time: 131.70 ms, Memory: 219 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] topping) {
+        
+        int answer = 0;
+        
+        Set<Integer> set = new HashSet();
+        HashMap<Integer, Integer> map = new HashMap();
+        
+        for(int i : topping){
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        
+        for(int i : topping){
+            set.add(i);
+            
+            map.put(i, map.get(i)-1);
+            
+            if(map.get(i)==0){
+                map.remove(i);
+            }
+            
+            if(set.size()==map.size()){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
