268857d435e3a1cb4b51272f84c198cd0ff47ec1
5castle0 <62434758+5castle0@users.noreply.github.com>
2026-07-30 10:30:43 +0900

    [level 2] Title: 주식가격, Time: 7.40 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        int len = prices.length;
+        int[] answer = new int[len];
+        
+        for(int i=0; i< len; i++){
+            int time = 0;
+            for(int j=i+1; j<len; j++){ 
+                time++;
+                
+                if(prices[i] > prices[j]){
+                   break;
+                }
+            }    
+            
+            answer[i] = time;
+        }
+        
+        return answer;
+    }
+}
